4e1b39d7df5af5145f18a8612433b48db4649e3e
Nishiguma <nisigma.200+github@gmail.com>
2026-02-17 01:36:34 +0900

    変更
    Googleアナリティクスが遮断されていたから避けるように。
    footerの年を自動更新

--- javascript/common.js
+++ javascript/common.js
@@ -6,10 +6,18 @@ pagetopBtn.addEventListener('click', () => {
   });
 });
 
-$(document).ready(function () {
-  $("#header").load("https://nisigma200.github.io/nisigma200/common/header.html");
-});
+/** 現在の実行環境におけるフル西暦（例：2026, 2027...） */
+const currentYear = new Date().getFullYear();
 
-$(document).ready(function () {
-  $("#footer").load("https://nisigma200.github.io/nisigma200/common/footer.html");
-});
+// 実行時の年が、あらかじめ設定した開始年よりも進んでいるかを確認する
+if (currentYear > 2026) {
+
+  /** 画面上の年を表示させたい要素（IDが copyright-year のもの） */
+  const displayTargetElement = document.getElementById('copyright-year');
+
+  // 指定した要素が存在する場合のみ、表示内容を更新する
+  if (displayTargetElement) {
+    // 開始年（2026）の後に、ハイフンと現在の年を連結して表示を上書きする
+    displayTargetElement.textContent = " - " + currentYear;
+  }
+}
